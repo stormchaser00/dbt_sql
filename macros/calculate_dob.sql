ae131ad1c304c7113ee16e6e402667795bac58d4
stormchaser00 <85726415+stormchaser00@users.noreply.github.com>
2021-08-30 09:30:30 +1200

    Create calculate_dob.sql

---
+++ macros/calculate_dob.sql
@@ -0,0 +1,23 @@
+create or replace function {{target.schema}}.datediffc(interval string, start_date date, end_date date)
+returns integer
+as 
+$$
+case 
+  when interval = 'years'
+  then
+    case 
+      when dateadd(year, datediff(years, start_date, end_date), start_date) > end_date
+      then datediff(years, start_date, end_date) - 1
+      else datediff(years, start_date, end_date)
+    end
+  when interval = 'months'
+  then
+    case
+      when dateadd(month, datediff(months, start_date, end_date), start_date) > end_date
+      then datediff(months, start_date, end_date) - 1
+      else datediff(months, start_date, end_date)
+    end
+  when interval = 'days'
+  then datediff(days, start_date, end_date)
+end
+$$
