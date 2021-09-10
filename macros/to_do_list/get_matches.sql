dd1c31ce49c6c536ca137aff19f18dc8d2410a84
stormchaser00 <85726415+stormchaser00@users.noreply.github.com>
2021-09-10 20:48:58 +1200

    Create get_matches.sql

---
+++ macros/to_do_list/get_matches.sql
@@ -0,0 +1,14 @@
+https://github.com/emilyriederer/dbt-convo-covid/tree/main/macros
+
+{% macro get_matches(input_list, regex) %}
+
+{% set results_list = [] %}
+{% for l in input_list %}
+    {% if modules.re.match(regex, l, modules.re.IGNORECASE) %}
+        {{ results_list.append(l) or "" }}
+    {% endif %}
+{% endfor %}
+
+{{ return(results_list) }}
+
+{% endmacro %}
